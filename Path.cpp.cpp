ec03a82c0d5bd10f91d207c3739844e153224384
Xin Chi Zhang <xin.c.zhang@uwaterloo.ca>
2017-07-18 19:50:40 -0400

    Upload new file

---
+++ Path.cpp.cpp
@@ -0,0 +1,61 @@
+#include "Path.h"
+#include "Address.h"
+
+Path::Path()
+{
+	this->number = 0;
+	this->numNeighborPathes = 0;
+	this->neighborPathes = new Path*[0];
+	this->owner = NULL;
+}
+
+Path::Path(int number):Path()
+{
+	this->number = number;
+}
+
+Path::~Path()
+{
+}
+
+Builder* Path::GetBuilder()
+{
+	return this->owner;
+}
+int Path::GetNumber()
+{
+	return this->number;
+}
+
+void Path::AddNeighborPath(Path* path)
+{
+	this->PushNeighborPath(path);
+	path->PushNeighborPath(this);
+}
+
+void Path::PushNeighborPath(Path* path)
+{
+	if (!this->isInNeighborPathes(path->GetNumber()))
+	{
+		Path** newArr = new Path*[this->numNeighborPathes + 1];
+		copy(this->neighborPathes, this->neighborPathes + this->numNeighborPathes, newArr);
+		delete[] this->neighborPathes;
+		this->neighborPathes = newArr;
+
+		this->neighborPathes[this->numNeighborPathes] = path;
+		this->numNeighborPathes++;
+	}
+}
+
+bool Path::isInNeighborPathes(int number)
+{
+	for (int i = 0;i<this->numNeighborPathes;i++)
+	{
+		if (this->neighborPathes[i]->GetNumber() == number)
+			return true;
+	}
+	return false;
+}
+
+
+
