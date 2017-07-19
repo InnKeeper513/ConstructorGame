a1bef241b0b14a3ff2bce078985f553dca30f48b
Ruijie Tang <r49tang@student.cs.uwaterloo.ca>
2017-07-19 09:23:02 -0400

    changed the main file

--- main.cpp.cpp
+++ main.cpp.cpp
@@ -18,6 +18,8 @@ int main()
 	file.close();
 	char  c;
 	cin >> c;
+	// random number for the dice
+	int rand;
 
 	// The beginning of game.
 	vecctor<Builders> builders;
@@ -26,12 +28,59 @@ int main()
 	builders.emplace_back(2,"Orange");
 	builders.emplace_back(3,"Yellow");
 
+	// Adding 2 initial address locations for the player
+	//TODO Address need to be valid
+	// - It must exist
+	// - It cannot collapse with other player's address
 	for(int i = 0; i < 4; i++){
+		int tempAdd;
 		cout << "Builder " << builders[i].getColor() << " , where do you want to build a basement?" << endl;
-		builders[i].
+		cin >> tempAdd;
+		builders[i].addAddress(tempAdd);
 	}
 	for(int j = 3; j >= 0; j++){
+		int tempAdd2;
+		cout << "Builder " << builders[i].getColor() << " , where do you want to build a basement?" << endl;
+		cin >> tempAdd2;
+		builders[i].addAddress(tempAdd2);
+	}
 
+	// Print the updated board
+	cout << board;
+
+	// Begin the game ********************
+
+	// Beginning of Turn *****************
+	// Loop through the game
+	while(true){
+
+		// Every turn, there will be 4 player inputing commands
+		for(int i = 0; i < 4; i++){
+			cout << "Builder " << builders[i].GetColor() << "'s turn'" << endl;
+			// print the builder's status
+			builders[i].status();
+
+			// Enter rolling dice commands
+			while(true){
+				string diceCMD;
+
+				cout << "Enter 'load' or 'fair' dice"
+				cin >> diceCMD;
+				Dice dice(diceCMD);
+				// Get the rolled number
+				rand = dice.roll();
+
+				if(diceCMD == "roll"){
+					break;
+				}
+			}
+
+	// End Beginning of Turn *********************
+	// Begin During the Turn *********************
+
+
+
+		}
 	}
 
 
