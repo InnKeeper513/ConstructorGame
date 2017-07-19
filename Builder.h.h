7aa09ad4f8d72a0e2429f42ad3abffe8e924d6e6
Ruijie Tang <r49tang@student.cs.uwaterloo.ca>
2017-07-19 07:54:48 -0400

    updated Builder

--- Builder.h.h
+++ Builder.h.h
@@ -1,23 +1,47 @@
 #pragma once
 #include <string>
-using namespace std;
+#include <vector>
+
+#ifndef _BUILDER_H_
+#define _BUILDER_H_
+
 class Builder
 {
 public:
-	Builder();
+	Builder(int number, std::string color, int numPoints=0, int numBrick=0,
+		int numEnergy=0, int numGlass=0, int numHeat=0, int numWifi=0);
 	~Builder();
 	int GetNumber();
-	string GetColor();
+	std::string GetColor();
 	int GetNumPoints();
 	int GetNumBrick();
 	int GetNumEnergy();
 	int GetNumGlass();
 	int GetNumHeat();
 	int GetNumWiFi();
+	// Added
+	int incrPoints(int);
+	int addBrick(int);
+	int addEnergy(int);
+	int addGlass(int);
+	int addHeat(int);
+	int addWifi(int);
+	int removeBrick(int);
+	int removeEnergy(int);
+	int removeGlass(int);
+	int removeHeat(int);
+	int removeWifi(int);
+	int addAddress(int);
+	int addPath(int);
+	vector<int> getAddress();
+	vector<int> getPath();
 
 private:
+
+	vector<int> address;
+	vector<int> path;
 	int number;
-	string color;
+	std::string color;
 	int numPoints;
 	int numBrick;
 	int numEnergy;
@@ -26,3 +50,4 @@ private:
 	int numWiFi;
 };
 
+#endif
